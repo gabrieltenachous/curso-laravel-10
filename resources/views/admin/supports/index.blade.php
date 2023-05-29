76b749422da956b2f240649205abbe44d0d73a74
root <root@DESKTOP-A0AT8PJ>
2023-05-29 16:11:14 -0300

    Models e Listagem de dados no blade Laravel 10

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,21 @@
 <h1>Listagem dos supports</h1>
+{!! $xss !!}}
+<table>
+    <thead>
+        <th>id</th>
+        <th>status</th>
+        <th>descrição</th>
+    </thead>
+    <tbody>
+        @foreach($supports as $suport)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@foreach($supports as $support)
+
+@endforeach
