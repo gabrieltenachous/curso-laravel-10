82db945f4d0dcc6e3ce2e8b17a5cfefd5dfc3938
root <root@DESKTOP-A0AT8PJ>
2023-06-04 15:17:25 -0300

    Paginacao Personalizada no Laravel

--- app/Services/SupportService.php
+++ app/Services/SupportService.php
@@ -6,6 +6,7 @@ use App\DTO\Supports\{
     CreateSupportDTO,
     UpdateSupportDTO
 };
+use App\Repositories\PaginationInterface;
 use App\Repositories\SupportRepositoryInterface;
 use stdClass;
 
@@ -14,7 +15,14 @@ class SupportService{
         protected SupportRepositoryInterface $repository,
     ) {
     }
-
+    public function paginate(int $page = 1, int $totalPerPage = 15, string $filter = null): PaginationInterface
+    {
+        return $this->repository->paginate(
+            page:$page,
+            totalPerPage:$totalPerPage,
+            filter:$filter
+        );
+    }
     public function getAll(string $filter = null) : array
     {
         return $this->repository->getAll($filter);
