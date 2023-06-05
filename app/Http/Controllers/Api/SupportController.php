1848b85f0fbd4bffd5c84833c8a03270a16a32a5
root <root@DESKTOP-A0AT8PJ>
2023-06-05 11:23:24 -0300

    API Rest no Laravel 10, listar registros com paginacao

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -22,9 +22,23 @@ class SupportController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $supports = $this->service->paginate(
+            page: $request->get('page', 1),
+            totalPerPage: $request->get('per_page', 15),
+            filter: $request->filter,
+        );
+        return SupportResource::collection($supports->items())->additional([
+            'meta' =>[
+                'total' => $supports->total(),
+                'is_first' => $supports->isFirstPage(),
+                'is_last_page' => $supports->isLastPage(),
+                'current_page' => $supports->currentPage(),
+                'next_page' => $supports->getNumberNextPage(),
+                'previous_page' => $supports->getNumberPreviousPage()
+            ]
+        ]);
     }
 
     /**
